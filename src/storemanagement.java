fce9ec8d8709729e24fcec86e2d6f889a4ef8fa2
hataejin <gkxowls7052@naver.com>
2022-03-16 11:04:57 +0900

    # WARNING: head commit changed in the meantime
    asd

---
+++ src/storemanagement.java
@@ -0,0 +1,13 @@
+
+public class storemanagement {
+	public static void main(String[] args) {
+		System.out.println("1. add store");
+		System.out.println("2. delete store");
+		System.out.println("3. edit store");
+		System.out.println("4. view store");
+		System.out.println("5. end");
+
+		
+	}
+
+}
