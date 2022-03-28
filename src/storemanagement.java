d32f61420b7a6d8921414667b29555815b9e5678
hataejin <gkxowls7052@naver.com>
2022-03-28 23:37:15 +0900

    recipe management plus methods

--- src/storemanagement.java
+++ src/storemanagement.java
@@ -5,22 +5,61 @@ public class storemanagement {
 		int a=0;
 		Scanner op = new Scanner(System.in);
 		while(a!=6) {
-			System.out.println("1. add store");
-			System.out.println("2. delete store");
-			System.out.println("3. edit store");
-			System.out.println("4. view store");
+			System.out.println("1. add recipe");
+			System.out.println("2. delete recipe");
+			System.out.println("3. edit recipe");
+			System.out.println("4. view recipe");
 			System.out.println("5. show menu");
 			System.out.println("6. end");
 			System.out.print("choose menu :");
 			a = op.nextInt();
 			if(a==1) {
-				System.out.print("storeid :");
-				int b = op.nextInt();
-				System.out.print("storename :");
-				String c = op.next();
+				addrecipe();
+			}
+			else if(a==2) {
+				deleterecipe();
+			}
+			else if(a==3) {
+				editrecipe();
+			}
+			else if(a==4) {
+				viewrecipe();
+			}
+			else {
+				continue;
 			}
 		}
 		
 	}
-
+	public static void addrecipe() {
+		Scanner ad = new Scanner(System.in);
+		System.out.print("recipe name :");
+		String b = ad.next();
+		System.out.print("ingredient :");
+		String c = ad.next();
+		System.out.println(c);
+		System.out.print("recipe :");
+		String d = ad.next();
+		System.out.println(d);
+	}
+	public static void deleterecipe() {
+		Scanner ad = new Scanner(System.in);
+		System.out.print("recipe name :");
+		String b = ad.next();
+	}
+	public static void editrecipe() {
+		Scanner ad = new Scanner(System.in);
+		System.out.print("recipe name :");
+		String b = ad.next();		
+	}
+	public static void viewrecipe() {
+		Scanner ad = new Scanner(System.in);
+		System.out.print("recipe name :");
+		String b = ad.next();		
+	}
+				
+			
+		
 }
+
+
